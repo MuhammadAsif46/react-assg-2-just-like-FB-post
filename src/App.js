6897863c0ae4f402741490e803cf353bc9b6c14b
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-26 01:52:18 +0500

    include index.html & styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="post-main">
+      <h1>{props.name}</h1>
+      <p>{props.date}</p>
+      <p>{props.bodyText}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App 
+    name="Muhammad Asif"
+    date="12-Jan-2022 3:50 pm"
+    bodyText="Quick warning though: this tutorial is compleeete. And by that I mean looong. I turned this into a full-fledged free 5-day course, and I made a nice-looking PDF you can read on your iPad or [whatever Android device is cool these days]. Drop your email in the box to get both. (it’s spread over 5 days, but you can jump ahead whenever you want)."
+    />
   </React.StrictMode>
 );
 
